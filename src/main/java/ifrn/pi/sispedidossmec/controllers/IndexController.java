1b1763a529a643c67ab0ea287efc9d4084e73d97
rickzada <rickjr2016@gmail.com>
2022-01-04 14:58:48 -0300

    Versão final

--- src/main/java/ifrn/pi/sispedidossmec/controllers/IndexController.java
+++ src/main/java/ifrn/pi/sispedidossmec/controllers/IndexController.java
@@ -8,7 +8,9 @@ public class IndexController {
 
 	@RequestMapping("/")
 	public String index() {
+		
 		return "home";
+		
 	}
 	
 }
--- src/main/java/ifrn/pi/sispedidossmec/security/WebSecurityConfig.java
+++ src/main/java/ifrn/pi/sispedidossmec/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -49,4 +50,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/img/**", "/error");
+	}
+	
 }
